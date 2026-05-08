977a634210c5520957c446200d3ba8bc6599fb48
simonfec <simonfec@tcd.ie>
2026-05-07 20:22:59 -0400

    wrote a little postfix bench calc

---
+++ src/parsing/calc.ml
@@ -0,0 +1,40 @@
+type op = 
+  | Value of int
+  | Unary of (int -> int) * op
+  | Binary of (int -> int -> int) * op * op
+
+let rec calc = function
+  | Value x -> x
+  | Unary (fn, x) -> fn (calc x)
+  | Binary (fn, x, y) -> fn (calc x) (calc y)
+;;
+
+let evalop = function
+| "+" -> (+)
+| "-" -> (-)
+| "*" -> ( * )
+| "/" -> (/)
+| _ -> failwith "bad op"
+;;
+
+let isop = function
+  | "+" | "-" | "*" | "/" -> true
+  | _ -> false
+
+let isdigit x =
+  match int_of_string_opt x with
+    | None -> false
+    | Some x -> true
+
+let parse_postfix toks =
+  let rec aux stack toks =
+    match stack, toks with
+      | x::_, [] -> calc x
+      | a::b::stk, t::ts when isop t-> aux (Binary (evalop t, a, b)::stk) ts
+      | stk, t::ts when isdigit t -> aux (Value (int_of_string t)::stk) ts
+      | _ -> failwith "Invalid Input"
+in aux [] toks
+
+let println_int x = Printf.printf "%d\n" x;;
+
+let () = parse_postfix ["3"; "5"; "+"; "8"; "*"] |> println_int;;
